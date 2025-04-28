d4ae054b60e012ffd1fe62d174f91b80cf5b3aa1
iantrn <ian.tornier@isen-ouest.yncrea.fr>
2025-04-28 11:20:09 +0200

    JSp

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 function main () {
-    function onCopy() {
-        alert("Toute édition d'écrits, de composition musicale, de dessin, de peinture ou de toute autre production, imprimée ou gravée en entier ou en partie, au mépris des lois et règlements relatifs à la propriété des auteurs, est une contrefaçon et toute contrefaçon est un délit.")
+    function onCopy() {  // envoyer une alerte + message dans la console 
+        alert("Toute édition d'écrits, de composition musicale, de dessin, de peinture ou de toute autre production, imprimée ou gravée en entier ou en partie, au mépris des lois et règlements relatifs à la propriété des auteurs, est une contrefaçon et toute contrefaçon est un délit.");
+        console.log("Toute édition d'écrits, de composition musicale, de dessin, de peinture ou de toute autre production, imprimée ou gravée en entier ou en partie, au mépris des lois et règlements relatifs à la propriété des auteurs, est une contrefaçon et toute contrefaçon est un délit.");
     }
-    document.addEventListener("copy", onCopy);
+    document.addEventListener("copy", onCopy); // envoit une alerte + message console si l'utilisateur copie quelque chose
+    function displayTime() {
+        document.querySelector("#timer").innerHTML = new Date().toLocaleTimeString("fr-fr");  // on écrit l'heure récupérée
+    }
+    setInterval(displayTime,1000); // on rafraichi l'heure toutes les secondes
+    function durationOnPage() {
+        let time_spent = 0;
+        while ()
+    }
 }
+
 
 main();
 
