99d59c02402085fcd5dc5857d0758ca8b3ec095a
iantrn <ian.tornier@isen-ouest.yncrea.fr>
2025-04-28 10:38:10 +0200

    Event Copy

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 function main () {
-    
+    function onCopy() {
+        alert("Toute édition d'écrits, de composition musicale, de dessin, de peinture ou de toute autre production, imprimée ou gravée en entier ou en partie, au mépris des lois et règlements relatifs à la propriété des auteurs, est une contrefaçon et toute contrefaçon est un délit.")
+    }
+    document.addEventListener("copy", onCopy);
 }
 
 main();
